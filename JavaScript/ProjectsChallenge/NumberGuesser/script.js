38c5b316eafabe95f742bd206bcae13f06cb4060
tobiasfr82 <tobiasfr82@gmail.com>
2020-10-16 18:50:09 +0200

    Update script.js
    Fixed code structure issues.

--- JavaScript/ProjectsChallenge/NumberGuesser/script.js
+++ JavaScript/ProjectsChallenge/NumberGuesser/script.js
@@ -4,21 +4,21 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
-const generateTarget 		= () 										=> Math.floor( Math.random() * 10 );
-const getAbsoluteDistance 	= ( guess, target ) 						=> distance = guess = Math.abs( target - guess );
+const generateTarget 		= () 						=> Math.floor( Math.random() * 10 );
+const getAbsoluteDistance 	= ( guess, target ) 				=> distance = guess = Math.abs( target - guess );
 const compareGuesses 		= ( humanGuess, computerGuess, target ) 	=> {
 
 	if ( humanGuess < 0 || humanGuess > 9 ) {
 		alert( 'Error: Wrong user input. Valid input are: one of the following numbers. 0, 1, 2, 3, 4, 5, 6, 7, 8, or 9.' );		
 	}
 
-	humanDifference 	= getAbsoluteDistance( humanGuess, 		target );
+	humanDifference 	= getAbsoluteDistance( humanGuess, 	target );
 	computerdifference 	= getAbsoluteDistance( computerGuess, 	target );
 
-	if 		( computerdifference 	> humanDifference 	 ) 	{ return true; 	}
-	else if ( computerdifference	< humanDifference 	 ) 	{ return false; }
+	if 	( computerdifference 	> humanDifference	) 	{ return true; 	}
+	else if ( computerdifference	< humanDifference	) 	{ return false; }
 	else 	{ return true; }			
 }
 
 const updateScore 	= winner 	=> winner === 'human' ? humanScore 	= humanScore + 1 : computerScore = computerScore + 1;
-const advanceRound 	= () 		=> currentRoundNumber 				= currentRoundNumber + 1;
+const advanceRound 	= () 		=> currentRoundNumber 			= currentRoundNumber + 1;
